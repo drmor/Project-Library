cbebd307f8bec8f1e3451bf3605de85df3d77c55
Miki D <69644623+drmor@users.noreply.github.com.>
2026-01-30 21:22:23 +0500

    Added a new button with remove and confirm buttons, for now work only first two, new button pop up a window with two buttons inside, centered div and did a little style

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const bookCard = document.querySelector(".card");
+const newBtn = document.getElementById("new");
 const myLibrary = [];
-let animal = { species: "dog" };
 
 function Book(title, author, pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.info = function(){
+        for (let i = 0; i < 3; i++) {
+            const bookInfo = document.createElement("p");
+            bookInfo.textContent = myLibrary[0][keys[i]];
+            bookCard.appendChild(bookInfo);
+        };
+    };
 };
 
 const book1 = new Book('Harry Potter', 'J. K. Rowling', '349');
@@ -13,8 +20,33 @@ myLibrary.push(book1);
 
 const keys = Object.keys(myLibrary[0]); 
 
-for (let i = 0; i < keys.length; i++) {
-  const bookInfo = document.createElement("p");
-  bookInfo.textContent = myLibrary[0][keys[i]];
-  bookCard.appendChild(bookInfo);
-}
+book1.info();
+
+newBtn.addEventListener('click', () => {
+    addNewBook();
+});
+
+function addNewBook(){
+    const popup = document.createElement("div");
+    const closeBtn = document.createElement("button");
+    const confirmBtn = document.createElement("button");
+    closeBtn.textContent = "close";
+    confirmBtn.textContent = "confirm";
+    popup.classList.add("popup");
+    document.body.appendChild(popup);
+    popup.appendChild(closeBtn);
+    popup.appendChild(confirmBtn);
+    function closeWindow(){
+        closeBtn.addEventListener('click', () => {
+            if(popup){
+                popup.remove();
+            };
+        });
+        confirmBtn.addEventListener('click', () => {
+            if(popup){
+                popup.remove();
+            };
+        });
+    };
+    closeWindow();
+};
